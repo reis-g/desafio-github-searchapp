af11023857dfd52b54465a9ee393867df54ca8cc
Guilherme Reis <161058845+reis-g@users.noreply.github.com>
2024-12-13 13:44:35 -0300

    Update Repository.kt

--- app/src/main/java/br/com/igorbag/githubsearch/domain/Repository.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/domain/Repository.kt
@@ -4,6 +4,5 @@ import com.google.gson.annotations.SerializedName
 
 data class Repository(
     val name: String,
-    @SerializedName("html_url")
-    val htmlUrl: String
+    val html_url: String
 )
